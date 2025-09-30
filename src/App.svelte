6b6fc7ea32f1469310fae1e2dabbb8176c29af21
Maxim Butenko <maxim.butenko@proton.me>
2025-09-30 16:41:40 +0300

    ready

--- src/App.svelte
+++ src/App.svelte
@@ -1,79 +1,79 @@
 <script lang="ts">
-    import Display from './lib/components/Display.svelte';
-    import Keyboard from './lib/components/Keyboard.svelte';
-    import Switcher from './lib/components/Switcher.svelte';
-    import { calculator } from './lib/state/Calculator.svelte';
-    import './styles/global.css';
+  import Display from './lib/components/Display.svelte';
+  import Keyboard from './lib/components/Keyboard.svelte';
+  import Switcher from './lib/components/Switcher.svelte';
+  import { calculator } from './lib/state/Calculator.svelte';
+  import './styles/global.css';
 
-    let theme = $state(0);
+  let theme = $state(0);
 </script>
 
-<div class="wrapper theme transition-color" data-theme={theme}>
-    <div class="wrapper-child">
-        <div class="keyboard-wrapper">
-            <div class="header">
-                <h1 class="logo">Calc</h1>
-                <Switcher
-                    name={'theme'}
-                    bind:value={theme}
-                    legend="Theme"
-                    srLegend="Select a theme variant"
-                    opts={[
-                        { value: 0, label: '1', srLabel: 'First' },
-                        { value: 1, label: '2', srLabel: 'Second' },
-                        { value: 2, label: '3', srLabel: 'Third' },
-                    ]}
-                />
-            </div>
-            <Display value={calculator.display} />
-            <Keyboard
-                onComma={calculator.addComma}
-                onBackspace={calculator.del}
-                onDigit={calculator.addDigit}
-                onEqual={calculator.equal}
-                onReset={calculator.reset}
-                onOp={calculator.setOp}
-            />
-        </div>
+<main class="wrapper theme transition-color" data-theme={theme}>
+  <div class="wrapper-child">
+    <div class="keyboard-wrapper">
+      <div class="header">
+        <h1 class="logo">Calc</h1>
+        <Switcher
+          name={'theme'}
+          bind:value={theme}
+          legend="Theme"
+          srLegend="Select a theme variant"
+          opts={[
+            { value: 0, label: '1', srLabel: 'First' },
+            { value: 1, label: '2', srLabel: 'Second' },
+            { value: 2, label: '3', srLabel: 'Third' },
+          ]}
+        />
+      </div>
+      <Display value={calculator.display} />
+      <Keyboard
+        onComma={calculator.addComma}
+        onBackspace={calculator.del}
+        onDigit={calculator.addDigit}
+        onEqual={calculator.equal}
+        onReset={calculator.reset}
+        onOp={calculator.setOp}
+      />
     </div>
-</div>
+  </div>
+</main>
 
 <style>
-    .wrapper {
-        --padding-inline: clamp(1.5rem, -3.87rem + 22.9vw, 7.125rem);
+  .wrapper {
+    --padding-inline: clamp(1.5rem, -3.87rem + 22.9vw, 7.125rem);
 
-        height: 100vh;
-        display: grid;
+    height: 100vh;
+    display: grid;
 
-        grid-template-columns: var(--padding-inline) 1fr var(--padding-inline);
-        grid-template-rows: 1fr max-content 1fr;
+    grid-template-columns: var(--padding-inline) 1fr var(--padding-inline);
+    grid-template-rows: 1fr max-content 1fr;
 
-        justify-items: center;
+    justify-items: center;
 
-        > .wrapper-child {
-            width: 100%;
-            grid-column: 2;
-            grid-row: 2;
-        }
+    > .wrapper-child {
+      width: 100%;
+      grid-column: 2;
+      grid-row: 2;
     }
+  }
 
-    .keyboard-wrapper {
-        max-width: 33.75rem;
-        margin-inline: auto;
-        display: grid;
-        row-gap: 1.5rem;
-    }
+  .keyboard-wrapper {
+    max-width: 33.75rem;
+    margin-inline: auto;
+    display: grid;
+    row-gap: 1.5rem;
+  }
 
-    .header {
-        display: flex;
-        justify-content: space-between;
-        align-items: flex-end;
-    }
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
 
-    .logo {
-        font-size: 2rem;
-        letter-spacing: 0.015625em;
-        text-transform: lowercase;
-        user-select: none;
-    }
+  .logo {
+    font-size: 2rem;
+    letter-spacing: 0.015625em;
+    text-transform: lowercase;
+    user-select: none;
+  }
 </style>
--- test.js
+++
@@ -1,7 +0,0 @@
-function delimit(input, d) {
-    let split = [];
-    for (let i = input.length; i > 0; i -= 3) {
-        split.unshift(input.substring(i - 3, i));
-    }
-    return split.join(d);
-}
